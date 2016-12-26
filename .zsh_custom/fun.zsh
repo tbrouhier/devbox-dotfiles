8acfeab6e7a40787fd113b117abfef80434d4eab
Pierre Radermecker <pradermecker@cirb.brussels>
2016-12-27 00:01:12 +0100

    Remove sshi function

--- .zsh_custom/fun.zsh
+++ .zsh_custom/fun.zsh
@@ -1,7 +1,3 @@
-function sshi () {
-	  ssh -A -i ~/.ssh/alhazen_rsa alhazen@$1
-}
-
 function presources () {
     puppetresources -p . -o "$1" --hiera ./tests/hiera.yaml --pdbfile tests/facts.yaml "${@:2}"
 }
